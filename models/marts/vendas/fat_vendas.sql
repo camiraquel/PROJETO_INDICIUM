6e10c837338f84875cbc45ac59dec6ed7602c01c
camila raquel cabral de souza <camiraquel.cabral@gmail.com>
2025-02-14 21:43:28 +0000

    criar fato

--- models/marts/vendas/fat_vendas.sql
+++ models/marts/vendas/fat_vendas.sql
@@ -1,65 +1,43 @@
 with
+    dim_solicitacao_venda as (
+        select *
+        from {{ ref("dim_solicitacao_venda") }}
+    ),
     dim_cartao_credito as (
         select *
-        from {{ ref("stg_salesorderheader") }}
+        from {{ ref("dim_cartao_credito") }}
     ),
-    detalhe_venda as (
+    dim_pessoa as (
         select *
-        from {{ ref("stg_salesorderdetail") }}
+        from {{ ref("dim_pessoa") }}
     ),
-    razao_venda as (
+    dim_endereco as (
         select *
-        from {{ ref("stg_salesorderheadersalesreason") }}
+        from {{ ref("dim_endereco") }}
     ),
-    razao as (
-        select *
-        from {{ ref("stg_salesreason") }}
-    ),
-    venda_incrementado as (
+    fat_vendas as (
         select
-            solicitacao_venda.pk_ordem_venda,
-            solicitacao_venda.numero_revisao,
-            solicitacao_venda.data_compra,
-            solicitacao_venda.data_vencimento,
-            solicitacao_venda.data_envio,
-            solicitacao_venda.status,
-            solicitacao_venda.ind_ordem_online,
-            solicitacao_venda.numero_ordem_compra,
-            solicitacao_venda.numero_conta,
-            solicitacao_venda.fk_cliente,
-            solicitacao_venda.fk_vendedor,
-            solicitacao_venda.fk_territorio,
-            solicitacao_venda.fk_endereco_cobranca,
-            solicitacao_venda.fk_endereco_envio,
-            solicitacao_venda.fk_metodo_envio,
-            solicitacao_venda.fk_cartao_credito,
-            solicitacao_venda.codigo_aprovacao_cartao_credito,
-            solicitacao_venda.fk_taxa_cambio,
-            solicitacao_venda.subtotal,
-            solicitacao_venda.taxamt,
-            solicitacao_venda.frete,
-            solicitacao_venda.total_devido,
-            solicitacao_venda.comentario,
-            solicitacao_venda.data_modificacao as data_modificacao_solicitacao_venda,
-            detalhe_venda.fk_detalhe_pedido_venda,
-            detalhe_venda.numero_rastreamento,
-            detalhe_venda.quantidade_pedido,
-            detalhe_venda.fk_produto,
-            detalhe_venda.fk_oferta_especial,
-            detalhe_venda.preco_unitario,
-            detalhe_venda.desconto_preco_unitario,
-            detalhe_venda.data_modificacao as data_modificacao_detalhe_venda,
-            razao_venda.pk_fk_razao_venda,
-            razao.nome,
-            razao.tipo_razao,
-            razao.data_modificacao as data_modificacao_razao
-        from solicitacao_venda
-        left join detalhe_venda
-            on solicitacao_venda.pk_ordem_venda = detalhe_venda.pk_pedido_venda
-        left join razao_venda
-            on solicitacao_venda.pk_ordem_venda = razao_venda.pk_fk_ordem_venda
-        left join razao
-            on razao_venda.pk_fk_razao_venda = razao.pk_razao_venda
+            dim_solicitacao_venda.pk_ordem_venda,
+            dim_solicitacao_venda.quantidade_pedido,
+            dim_solicitacao_venda.fk_produto,
+            dim_solicitacao_venda.preco_unitario, 
+            dim_solicitacao_venda.desconto_preco_unitario,
+            dim_solicitacao_venda.fk_cartao_credito,
+            dim_solicitacao_venda.pk_fk_razao_venda,
+            dim_solicitacao_venda.data_compra,
+            dim_pessoa.pk_fk_entidade_negocio,
+            dim_solicitacao_venda.status,
+            dim_solicitacao_venda.fk_endereco_envio,
+            dim_endereco.pk_estado,
+            dim_endereco.pk_codigo_pais,
+            dim_solicitacao_venda.subtotal
+        from dim_solicitacao_venda
+        left join dim_cartao_credito
+            on dim_solicitacao_venda.fk_cartao_credito = dim_cartao_credito.pk_cartao_credito
+        left join dim_pessoa
+            on dim_cartao_credito.pk_entidade_negocio = dim_pessoa.pk_fk_entidade_negocio
+        left join dim_endereco
+            on dim_solicitacao_venda.fk_endereco_envio = dim_endereco.pk_endereco
     )
 select * 
-from venda_incrementado
+from fat_vendas
